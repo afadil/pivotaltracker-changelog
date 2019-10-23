90b28fe682728be032874cdb7b839c88313523ed
Aziz FADIL <aziz.fadil@gmail.com>
2019-10-23 16:49:29 -0400

    update dependencies

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,11 +1,11 @@
-import {createStore, applyMiddleware} from 'redux';
-import rootReducer from '../reducers/rootReducer';
+import { createStore, applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import thunk from 'redux-thunk';
+import reducer from '../reducers/rootReducer';
 
 export default function configureStore() {
-  return createStore(
-    rootReducer,
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
-    applyMiddleware(thunk)
-  );
+  const enhancer = composeWithDevTools(applyMiddleware(thunk));
+  const store = createStore(reducer, enhancer);
+  return store;
 }
+
